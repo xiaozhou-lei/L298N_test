4480ad963a7bb4456965547c93cb0a45a384120a
joey <joey@emakefun.com>
2022-04-22 11:20:17 +0800

    create

---
+++ msg.js
@@ -0,0 +1,47 @@
+// This file was automatically generated.  Do not modify.
+/* eslint-disable func-style */
+/* eslint-disable require-jsdoc */
+/* eslint-disable quote-props */
+/* eslint-disable quotes */
+/* eslint-disable dot-notation */
+
+function addMsg (Blockly) {
+
+    Object.assign(Blockly.ScratchMsgs.locales["en"],
+        {
+            "L298N_BACK": "back",
+            "L298N_CATEGORY": "L298N",
+            "L298N_FORWARD": "foward",
+            "L298N_INIT": "init channel %1 pin IN1 %2 IN2 %3 EN %4",
+            "L298N_RUN": "channel %1 run %2 speed %3",
+            "L298N_STOP": "channel %1 stop"
+        }
+    );
+
+    Object.assign(Blockly.ScratchMsgs.locales["zh-cn"],
+        {
+            "L298N_BACK": "back",
+            "L298N_CATEGORY": "L298N",
+            "L298N_FORWARD": "foward",
+            "L298N_INIT": "init channel %1 pin IN1 %2 IN2 %3 EN %4",
+            "L298N_RUN": "channel %1 run %2 speed %3",
+            "L298N_STOP": "channel %1 stop"
+        }
+    );
+
+    Object.assign(Blockly.ScratchMsgs.locales["zh-tw"],
+        {
+            "L298N_BACK": "back",
+            "L298N_CATEGORY": "L298N",
+            "L298N_FORWARD": "foward",
+            "L298N_INIT": "init channel %1 pin IN1 %2 IN2 %3 EN %4",
+            "L298N_RUN": "channel %1 run %2 speed %3",
+            "L298N_STOP": "channel %1 stop"
+        }
+    );
+
+    return Blockly;
+}
+
+exports = addMsg;
+// End of combined translations
