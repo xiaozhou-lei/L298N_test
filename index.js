3b80202dfad02d2bde1c065f80dc62c48152e2ba
xiaozhou-lei <61865102+xiaozhou-lei@users.noreply.github.com>
2022-04-24 21:33:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const l298n = formatMessage => ({
-    name: 'L298N',
-    extensionId: 'l298n',
+    name: 'L298N1',
+    extensionId: 'l298n1',
     version: '1.0.0',
-    supportDevice: ['arduinoUno', 'arduinoNano', 'arduinoMini', 'arduinoLeonardo',
+    supportDevice: ['arduinoUno', 'arduinoNano', 'nulllab', 'arduinoLeonardo',
         'arduinoMega2560', 'arduinoEsp8266'],
     author: 'Liang',
     iconURL: `asset/l298n.png`,
